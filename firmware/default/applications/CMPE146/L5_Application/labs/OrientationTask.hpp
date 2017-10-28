14036ed7ab629bb2c26099917f2a1b9adec76f69
JP <jpan127@gmail.com>
2017-10-28 13:41:04 -0700

    Cleaned up orientation tasks

--- firmware/default/applications/CMPE146/L5_Application/labs/OrientationTask.hpp
+++ firmware/default/applications/CMPE146/L5_Application/labs/OrientationTask.hpp
@@ -9,21 +9,23 @@
 #include "L5_Application/drivers/led.hpp"
 #include "L5_Application/drivers/accelerometer.hpp"
 
-typedef enum { UP, DOWN, LEFT, RIGHT } 	orientation_t;
-typedef enum { SHARED_QUEUE_HANDLE } 	shared_handle_id_t;
+#define DELAY(ms) (vTaskDelay(ms / portTICK_PERIOD_MS))
+
+typedef enum { UP, DOWN, LEFT, RIGHT }  orientation_t;
+typedef enum { SHARED_QUEUE_HANDLE }    shared_handle_id_t;
 
 typedef struct
 {
-	orientation_t x;
-	orientation_t y;
+    orientation_t x;
+    orientation_t y;
 } __attribute__((packed)) orientation_xy_t;
 
 typedef struct 
 { 
-	int16_t				x;
-	int16_t				y;
-	int16_t				z;
-	orientation_xy_t 	orientation;
+    int16_t             x;
+    int16_t             y;
+    int16_t             z;
+    orientation_xy_t    orientation;
 } __attribute__((packed)) axis_position_t;
 
 // When OrientationProcessTask is higher priority than OrientationGetTask,
@@ -43,41 +45,48 @@ class OrientationGetTask : public scheduler_task
 {
 public:
 
-	OrientationGetTask(uint8_t priority) : scheduler_task("OrientationGetTask", 2048, priority)
-	{
-		Queue = xQueueCreate(1, sizeof(Axis));
-		addSharedObject(SHARED_QUEUE_HANDLE, Queue);
-	}
+    OrientationGetTask(uint8_t priority) : scheduler_task("OrientationGetTask", 2048, priority)
+    {
+        Queue = xQueueCreate(1, sizeof(Axis));
+        addSharedObject(SHARED_QUEUE_HANDLE, Queue);
+    }
 
-	bool run(void* p)
-	{
-		if ( Button0::getInstance().IsPressed() ) 
-		{
-			Axis.x = Accelerometer::getInstance().GetX();
-			Axis.y = Accelerometer::getInstance().GetY();
-			Axis.z = Accelerometer::getInstance().GetZ();
+    void SendToQueue()
+    {
+        Axis.x = Accelerometer::getInstance().GetX();
+        Axis.y = Accelerometer::getInstance().GetY();
+        Axis.z = Accelerometer::getInstance().GetZ();
 
-			Axis.orientation.x = (Axis.x > 0) ? (LEFT) : (RIGHT);
-			Axis.orientation.y = (Axis.y > 0) ? (UP)   : (DOWN);
+        Axis.orientation.x = (Axis.x > 0) ? (LEFT) : (RIGHT);
+        Axis.orientation.y = (Axis.y > 0) ? (UP)   : (DOWN);
 
-			puts("[OrientationGetTask] Before send.");
+        puts("[OrientationGetTask] Before send.");
 
-			if ( !(xQueueSend(Queue, &Axis, portMAX_DELAY)) )
-			{
-				puts("[OrientationGetTask] Failed to send.");
-			}
-			
-			puts("[OrientationGetTask] After send.");
-		}
+        if ( !(xQueueSend(Queue, &Axis, portMAX_DELAY)) )
+        {
+            puts("[OrientationGetTask] Failed to send.");
+        }
+        
+        puts("[OrientationGetTask] After send.");
+    }
 
-		vTaskDelay(100);
-		return true;
-	}
+    bool run(void* p)
+    {
+        // if ( Button0::getInstance().IsPressed() ) 
+        // {
+        //     SendToQueue();
+        // }
+
+        SendToQueue();
+        DELAY(1000);
+
+        return true;
+    }
 
 private:
 
-	QueueHandle_t 	Queue;
-	axis_position_t	Axis;
+    QueueHandle_t   Queue;
+    axis_position_t Axis;
 };
 
 
@@ -88,56 +97,50 @@ class OrientationProcessTask : public scheduler_task
 {
 public:
 
-	OrientationProcessTask (uint8_t priority) : scheduler_task("OrientationProcessTask", 2048, priority)
-	{
-		Queue = NULL;
-	}
+    OrientationProcessTask (uint8_t priority) : scheduler_task("OrientationProcessTask", 2048, priority)
+    {
+        Queue = NULL;
+    }
 
-	bool run(void* p)
-	{
-		Queue = getSharedObject(SHARED_QUEUE_HANDLE);
+    bool run(void* p)
+    {
+        Queue = getSharedObject(SHARED_QUEUE_HANDLE);
 
-		if ( !(xQueueReceive(Queue, &Axis, portMAX_DELAY)) )
-		{
-			puts("[OrientationProcessTask] Failed to receive.");
-		}
+        if ( !(xQueueReceive(Queue, &Axis, portMAX_DELAY)) )
+        {
+            puts("[OrientationProcessTask] Failed to receive.");
+        }
 
-		printf("X: %i | Y: %i | Z: %i\n", Axis.x, Axis.y, Axis.z);
+        printf("X: %i | Y: %i | Z: %i\n", Axis.x, Axis.y, Axis.z);
 
-		switch (Axis.orientation.y)
-		{
-			case UP: 	printf("UP   | "); 	break;
-			case DOWN: 	printf("DOWN | "); 	break;
-			default:						break;
-		}
-		switch (Axis.orientation.x)
-		{
-			case LEFT:	printf("LEFT\n"); 	break;
-			case RIGHT: printf("RIGHT\n"); 	break;
-			default:						break;
-		}
+        switch (Axis.orientation.y)
+        {
+            case UP:    printf("UP   | ");  break;
+            case DOWN:  printf("DOWN | ");  break;
+            default:                        break;
+        }
+        switch (Axis.orientation.x)
+        {
+            case LEFT:  printf("LEFT\n");   break;
+            case RIGHT: printf("RIGHT\n");  break;
+            default:                        break;
+        }
 
-		LightUpLeds();
+        LightUpLeds();
 
-		return true;
-	}
+        return true;
+    }
 
-	void LightUpLeds()
-	{
-		Led0::getInstance().ClearLed();
-		Led1::getInstance().ClearLed();
-		Led2::getInstance().ClearLed();
-		Led3::getInstance().ClearLed();
+    void LightUpLeds()
+    {
+        LedsClearAll();
 
-		(Axis.orientation.x == LEFT) ? (Led0::getInstance().SetLed()) : (Led3::getInstance().SetLed());
-		(Axis.orientation.y == UP)   ? (Led1::getInstance().SetLed()) : (Led2::getInstance().SetLed());
-	}
+        (Axis.orientation.x == LEFT) ? (Led0::getInstance().SetLed()) : (Led3::getInstance().SetLed());
+        (Axis.orientation.y == UP)   ? (Led1::getInstance().SetLed()) : (Led2::getInstance().SetLed());
+    }
 
 private:
 
-	QueueHandle_t 	Queue;
-	axis_position_t	Axis;
+    QueueHandle_t   Queue;
+    axis_position_t Axis;
 };
-
-
-
