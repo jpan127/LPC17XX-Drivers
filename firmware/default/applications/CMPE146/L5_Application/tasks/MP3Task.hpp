fc5c627d3791c479c5c719941577255a9c5ac5c9
Jonathan Pan <jpan127@users.noreply.github.com>
2017-12-01 23:54:23 -0800

    MVC

--- firmware/default/applications/CMPE146/L5_Application/tasks/MP3Task.hpp
+++ firmware/default/applications/CMPE146/L5_Application/tasks/MP3Task.hpp
@@ -186,24 +186,162 @@ static void CheckButtons()
 
 void MP3Task(void *p)
 {
-    // Initialize status struct
-    Status.cancel_requested = false;
-    Status.next_state = IDLE;
-
     memset(Buffer, 0, sizeof(Buffer));
     ssp0_init(0);
-    ssp0_set_max_clock(2);
-    MP3Player.SystemInit();
 
+    const uint8_t reset = 30;
+    const uint8_t dreq  = 19;
+    const uint8_t xcs   = 29;
+    const uint8_t xdcs  = 0;
+
+    // Set direction
+    LPC_GPIO0->FIODIR |= (1 << reset);
+    LPC_GPIO0->FIODIR &= ~(1 << dreq);
+    LPC_GPIO0->FIODIR |= (1 << xcs);
+    LPC_GPIO0->FIODIR |= (1 << xdcs);
+
+    // Set initial state
+    LPC_GPIO0->FIOSET |= (1 << xcs);
+    LPC_GPIO0->FIOSET |= (1 << xdcs);
+    LPC_GPIO0->FIOCLR |= (1 << reset);
+    vTaskDelay(1 / portTICK_PERIOD_MS);
+    LPC_GPIO0->FIOSET |= (1 << reset);
+    while ( !(LPC_GPIO0->FIOPIN & (1 << dreq)) );
+
+    const uint16_t default_status = 0x4846; // line1, native spi, stream, reset, allow mpeg 1+2
+    const uint16_t default_clock  = 0x6000;
+
+    // CLOCKF
+    while ( !(LPC_GPIO0->FIOPIN & (1 << dreq)) );
+    LPC_GPIO0->FIOCLR |= (1 << xcs);
+    ssp0_exchange_byte(0x02);
+    ssp0_exchange_byte(0x3);
+    ssp0_exchange_byte(default_status >> 8);
+    ssp0_exchange_byte(default_status & 0xFF);
+    LPC_GPIO0->FIOSET |= (1 << xcs);
+    // MODE
+    while ( !(LPC_GPIO0->FIOPIN & (1 << dreq)) );
+    LPC_GPIO0->FIOCLR |= (1 << xcs);
+    ssp0_exchange_byte(0x02);
+    ssp0_exchange_byte(0x0);
+    ssp0_exchange_byte(default_status >> 8);
+    ssp0_exchange_byte(default_status & 0xFF);
+    LPC_GPIO0->FIOSET |= (1 << xcs);
+
+    // Wait for button trigger
+    while ( !(Button0::getInstance().IsPressed()) );
+
+    // Main loop
     while (1)
     {
-        CheckButtons();
-        HandleStateLogic();
-        vTaskDelay(50 / portTICK_PERIOD_MS);
+        // Open mp3 file
+        const char name[] = "1:track1.mp3"
+        result = f_open(&mp3_file, name, FA_OPEN_EXISTING | FA_READ);
+        if (result != FR_OK)
+        {
+            printf("[MP3Task] mp3 file failed to open. Error: %d\n", result);
+            break;
+        }
+        else
+        {
+            printf("[MP3Task] %s successfully opened.\n", name);
+        }
+        // DECODE_TIME
+        while ( !(LPC_GPIO0->FIOPIN & (1 << dreq)) );
+        LPC_GPIO0->FIOCLR |= (1 << xcs);
+        ssp0_exchange_byte(0x02);
+        ssp0_exchange_byte(0x4);
+        ssp0_exchange_byte(0);
+        ssp0_exchange_byte(0);
+        LPC_GPIO0->FIOSET |= (1 << xcs);
+        // DECODE_TIME
+        while ( !(LPC_GPIO0->FIOPIN & (1 << dreq)) );
+        LPC_GPIO0->FIOCLR |= (1 << xcs);
+        ssp0_exchange_byte(0x02);
+        ssp0_exchange_byte(0x4);
+        ssp0_exchange_byte(0);
+        ssp0_exchange_byte(0);
+        LPC_GPIO0->FIOSET |= (1 << xcs);
+
+        // Read a segment at a time before sending to device
+        current_segment_size = 0;
+
+        // While reading max segment size, keep going
+        while (current_segment_size < MP3_SEGMENT_SIZE)
+        {
+            // Read a segment
+            result = f_read(&mp3_file, Buffer, MP3_SEGMENT_SIZE, &current_segment_size);
+            if (result != FR_OK)
+            {
+                printf("[MP3Task] mp3 file failed to read. Error: %d\n", result);
+                break;
+            }
+
+            uint32_t cycles    = current_segment_size / 32;
+            uint16_t remainder = current_segment_size % 32;
+
+            // Wait for DREQ
+            while ( !(LPC_GPIO0->FIOPIN & (1 << dreq)) );
+            // XDCS LOW
+            LPC_GPIO0->FIOCLR |= (1 << xdcs);
+
+            // Send bytes for each cycle
+            for (uint32_t cycle=0; cycle<cycles; cycle++)
+            {
+                for (int byte=0; byte<32; byte++)
+                {
+                    ssp0_exchange_byte(data[cycle * 32 + byte]);
+                }
+                // Wait for DREQ
+                while ( !(LPC_GPIO0->FIOPIN & (1 << dreq)) );
+            }
+
+            // Send remainder
+            if (remainder > 0)
+            {
+                for (int byte=0; byte<remainder; byte++)
+                {
+                    // Cycles instead of cycle because it is after the last cycle
+                    ssp0_exchange_byte(data[cycles * 32 + byte]);
+                }
+                // Wait for DREQ
+                while ( !(LPC_GPIO0->FIOPIN & (1 << dreq)) );
+            }
+
+            // XDCS HIGH
+            LPC_GPIO0->FIOSET |= (1 << xdcs);
+        }
+
+        // Close the damn file
+        result = f_close(&mp3_file);
+        if (result != FR_OK) printf("[MP3Task] mp3 file failed to close!\n");
+        else                 printf("[MP3Task] mp3 file closed.\n");
+
+        // Wait 10 seconds
+        vTaskDelay(10 * 1000 / portTICK_PERIOD_MS);
     }
 
-    // Should use logic analyzer on LPC's SPI output
+    printf("[MP3Task] MP3 file failed so task is suspending...\n");
+    vTaskSuspend(NULL);
 }
+
+// void MP3Task(void *p)
+// {
+//     // Initialize status struct
+//     Status.cancel_requested = false;
+//     Status.next_state = IDLE;
+
+//     memset(Buffer, 0, sizeof(Buffer));
+//     ssp0_init(0);
+//     MP3Player.SystemInit();
+
+//     while (1)
+//     {
+//         CheckButtons();
+//         HandleStateLogic();
+//         vTaskDelay(50 / portTICK_PERIOD_MS);
+//     }
+// }
 
 // class MP3Task : public scheduler_task
 // {
